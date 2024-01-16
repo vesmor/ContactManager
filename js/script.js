78e32d0cffac5f300407d8a1ef5124431917d229
deandrehendrix <deandrehendrix96@gmail.com>
2024-01-15 21:18:21 -0500

    Added log in, sing up, and contacts manager initial pages

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// JavaScript functionality can be added here
+
+// Example function to display a greeting alert
+function sayHello() {
+  alert("Hello, welcome to my website!");
+}
+
+// You can add more functions and variables as needed
