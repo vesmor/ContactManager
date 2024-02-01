f7cf287cf3e745e9e70c321a0a9a94fdccd48431
vesmor <romsev.charles@gmail.com>
2024-02-01 06:00:27 -0500

    redid innerHTML as given string

--- js/code.js
+++ js/code.js
@@ -364,36 +364,34 @@ function populateContacts(contacts) {
             .getElementById("discardEditedContactBtn")
             .addEventListener("click", async function(){
 
-              document.getElementById("contactDetails").reset();
-
-            //   //switch back to the non-editable contact details
-            //   contactDetailsElement.innerHTML =  ` <div class="text-right mb-2">
-            //   <button id="editContactBtn" class="btn btn-primary">Edit</button>
-            //   <button id="deleteContactBtn" class="btn btn-danger" data-contact-id="${contact.ID}">Delete</button>
-            //   </div>
-            //   <div class="row justify-content-center text-center mb-4">
-            //       <div class="col">
-            //           <img
-            //               id="contactImage"
-            //               src="images/default_img.png"
-            //               alt="Contact Image"
-            //               class="rounded-circle"
-            //               style="width: 200px; height: 200px; object-fit: cover"
-            //           />
-            //           <h2 id="contactName">${contact.FirstName} ${contact.LastName}</h2>
-            //       </div>
-            //   </div>
-            //   <div class="row">
-            //       <div class="col-6 text-left">
-            //           <p id="contactPhone" class="contact-detail">
-            //               <strong>Phone:</strong> ${contact.Phone}
-            //           </p>
-            //           <p id="contactEmail" class="contact-detail">
-            //               <strong>Email:</strong> ${contact.Email}
-            //           </p>
-            //       </div>
-            //   </div>
-            // `;
+              //switch back to the non-editable contact details
+              contactDetailsElement.innerHTML =  ` <div class="text-right mb-2">
+              <button id="editContactBtn" class="btn btn-primary">Edit</button>
+              <button id="deleteContactBtn" class="btn btn-danger" data-contact-id="${contact.ID}">Delete</button>
+              </div>
+              <div class="row justify-content-center text-center mb-4">
+                  <div class="col">
+                      <img
+                          id="contactImage"
+                          src="images/default_img.png"
+                          alt="Contact Image"
+                          class="rounded-circle"
+                          style="width: 200px; height: 200px; object-fit: cover"
+                      />
+                      <h2 id="contactName">${contact.FirstName} ${contact.LastName}</h2>
+                  </div>
+              </div>
+              <div class="row">
+                  <div class="col-6 text-left">
+                      <p id="contactPhone" class="contact-detail">
+                          <strong>Phone:</strong> ${contact.Phone}
+                      </p>
+                      <p id="contactEmail" class="contact-detail">
+                          <strong>Email:</strong> ${contact.Email}
+                      </p>
+                  </div>
+              </div>
+            `;
             
             })
           ;
